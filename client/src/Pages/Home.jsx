9d08188049e653e740d278dd5b9a45cb5ef67ecc
Cami <98415999+CamilaG17@users.noreply.github.com>
2025-03-28 18:58:23 -0400

    Client Homepage for bookstore
    I will add functionality based on our features.

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+            <div className="home-container">
+              <div className="featured-section">
+                <h2>Featured Scrolls</h2>
+                <div className="book-grid">
+                  <div className="book-card">
+                    <div className="book-thumbnail"></div>
+                    <h3>The Medieval Chronicles</h3>
+                    <p>Author Name</p>
+                    <p>Price: $19.99</p>
+                  </div>
+                  <div className="book-card">
+                    <div className="book-thumbnail"></div>
+                    <h3>Ancient Scrolls</h3>
+                    <p>Author Name</p>
+                    <p>Price: $24.99</p>
+                  </div>
+                  <div className="book-card">
+                    <div className="book-thumbnail"></div>
+                    <h3>Knights of the Realm</h3>
+                    <p>Author Name</p>
+                    <p>Price: $21.99</p>
+                  </div>
+                  <div className="book-card">
+                    <div className="book-thumbnail"></div>
+                    <h3>Knights of the Realm</h3>
+                    <p>Author Name</p>
+                    <p>Price: $21.99</p>
+                  </div>
+                  <div className="book-card">
+                    <div className="book-thumbnail"></div>
+                    <h3>Knights of the Realm</h3>
+                    <p>Author Name</p>
+                    <p>Price: $21.99</p>
+                  </div>
+                  <div className="book-card">
+                    <div className="book-thumbnail"></div>
+                    <h3>Knights of the Realm</h3>
+                    <p>Author Name</p>
+                    <p>Price: $21.99</p>
+                  </div>
+                  <div className="book-card">
+                    <div className="book-thumbnail"></div>
+                    <h3>Knights of the Realm</h3>
+                    <p>Author Name</p>
+                    <p>Price: $21.99</p>
+                  </div>
+                  <div className="book-card">
+                    <div className="book-thumbnail"></div>
+                    <h3>Knights of the Realm</h3>
+                    <p>Author Name</p>
+                    <p>Price: $21.99</p>
+                  </div>
+                  <div className="book-card">
+                    <div className="book-thumbnail"></div>
+                    <h3>Knights of the Realm</h3>
+                    <p>Author Name</p>
+                    <p>Price: $21.99</p>
+                  </div>
+                  <div className="book-card">
+                    <div className="book-thumbnail"></div>
+                    <h3>Knights of the Realm</h3>
+                    <p>Author Name</p>
+                    <p>Price: $21.99</p>
+                  </div>
+                  <div className="book-card">
+                    <div className="book-thumbnail"></div>
+                    <h3>Knights of the Realm</h3>
+                    <p>Author Name</p>
+                    <p>Price: $21.99</p>
+                  </div>
+                  <div className="book-card">
+                    <div className="book-thumbnail"></div>
+                    <h3>Knights of the Realm</h3>
+                    <p>Author Name</p>
+                    <p>Price: $21.99</p>
+                  </div>
+                 
+                </div>
+              </div>
+            </div>
+          );
+        };
+
+export default Home;
