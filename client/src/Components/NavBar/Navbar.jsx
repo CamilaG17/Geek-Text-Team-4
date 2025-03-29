25a0c1b52b20c0133a51a97a2a8bf1db1051b2e2
Bruna <Brunic9899@gmail.com>
2025-03-29 19:26:05 -0400

    changed src={logo_icon} to src=/logo.png

--- client/src/Components/NavBar/Navbar.jsx
+++ client/src/Components/NavBar/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Navbar.css' // Importing the CSS file for the Navbar component
-import logo_icon from '/logo.png'; // Importing the logo icon
-import search_icon from '/search.png'; // Importing the search icon
-import wishlist_icon from '/wishlist.png'; // Importing the notification icon
-import profile_icon from '/profile.png'; // Importing the user profile icon
-import cart_icon from '/Cart.png'; // Importing the cart icon
+//import logo_icon from '/logo.png'; // Importing the logo icon
+//import search_icon from '/search.png'; // Importing the search icon
+//import wishlist_icon from '/wishlist.png'; // Importing the notification icon
+//import profile_icon from '/profile.png'; // Importing the user profile icon
+//import cart_icon from '/Cart.png'; // Importing the cart icon
 
 // Navbar component definition
 const Navbar = () => {
@@ -17,28 +17,28 @@ const Navbar = () => {
         /* I moved the logos to public folder cause thats convention.
         Also the convention is to inline like src="/logo.png" instead of importing it like we did before. */ 
       }
-        <img className='Logo-icon' src={logo_icon} alt="Logo Icon" /> 
+        <img className='Logo-icon' src="logo.png" alt="Logo Icon" /> 
       </div>
   
       <div className="nav-middle flex-div">
         <div className="search-box flex-div">
           <input className = 'Search' type="text" placeholder='Search by Title, Author or ISBN'  style={{ WebkitTextFillColor: '#3E2723', textAlign: 'left', display: 'flex',alignItems: 'center', height: '30px'}}  /> 
-          <img src={search_icon} alt="Search Icon" /> 
+          <img src="search.png" alt="Search Icon" /> 
         </div>
       </div> 
 
 
       <div className="nav-right">
     <div className="wishlist-container">
-        <img className="wishlist" src={wishlist_icon} alt="Wishlist Icon" />
+        <img className="wishlist" src="wishlist.png" alt="Wishlist Icon" />
         <span className="wishlist-text">Wishlist</span>
     </div>
     <div className="profile-container">
-        <img className="profile" src={profile_icon} alt="Profile Icon" />
+        <img className="profile" src="profile.png" alt="Profile Icon" />
         <span className="profile-text">Profile</span>
     </div>
     <div className ="cart-container">
-      <img className ="cart" src={cart_icon} alt="Cart Icon"/>
+      <img className ="cart" src="cart.png" alt="Cart Icon"/>
       <span className ="cart-text">Cart</span>
     </div>
 </div>
