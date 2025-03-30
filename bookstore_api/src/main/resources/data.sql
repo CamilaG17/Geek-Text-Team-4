0eaede908a684a0e0cc5bae2cb2faf1eeca0fc0a
brian235 <briangarica19bgg@gmail.com>
2025-03-30 12:02:38 -0400

    Deleted my old api folder

--- bookstore_api/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO book (title, author, genre, price, copies_sold, rating, publisher)
-VALUES ('The Hobbit', 'J.R.R. Tolkien', 'Fantasy', 15.99, 5000000, 4.8, 'HarperCollins');
-
-INSERT INTO book (title, author, genre, price, copies_sold, rating, publisher)
-VALUES ('Harry Potter', 'J.K. Rowling', 'Fantasy', 12.99, 10000000, 4.9, 'Bloomsbury');
