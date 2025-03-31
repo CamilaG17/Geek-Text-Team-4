49b0207cb72b866bdc367d83a0560e4706c4d50d
Ari <arlenehdez0225@gmail.com>
2025-03-30 23:07:23 -0400

    Remove unused import

--- server/src/main/java/com/example/BookStore/BookRating/ratingService.java
+++ server/src/main/java/com/example/BookStore/BookRating/ratingService.java
@@ -3,6 +3,7 @@ package com.example.BookStore.BookRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import java.util.List;
 import java.util.OptionalDouble;
 
